57f8f02b5b980c63909a6cc3fc21f8e85f94c98f
MarcelPiNacy <marcelpi97@gmail.com>
2020-12-05 00:11:28 +0100

    Improved network for N=6.
    Source: http://users.telenet.be/bertdobbelaere/SorterHunter/sorting_networks.html

--- osn.hpp
+++ osn.hpp
@@ -129,11 +129,11 @@ namespace osn
 		{
 			static constexpr index_pair swap_sequence[] =
 			{
-				{ 0, 1 }, { 2, 3 }, { 4, 5 },
-				{ 0, 2 }, { 3, 5 }, { 1, 4 },
+				{ 0, 5 }, { 1, 3 }, { 2, 4 },
+				{ 1, 2 }, { 3, 4 },
+				{ 0, 3 }, { 2, 5 },
 				{ 0, 1 }, { 2, 3 }, { 4, 5 },
 				{ 1, 2 }, { 3, 4 },
-				{ 2, 3 }
 			};
 
 			static constexpr size_t depth = 5;

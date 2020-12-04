9df6682f9a70d508ee292e01029483309f8e20bc
MarcelPiNacy <marcelpi97@gmail.com>
2020-12-04 16:18:22 +0100

    Minor formatting changes, moved license to .HPP and updated gitignore.

--- osn.hpp
+++ osn.hpp
@@ -1,8 +1,30 @@
+/*
+	MIT License
+	
+	Copyright (c) 2020 Marcel Pi Nacy
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
 #pragma once
 #include <iterator>
 #include <cassert>
-
-
 
 namespace osn
 {
@@ -170,8 +192,8 @@ namespace osn
 
 		if constexpr (N > 1)
 		{
-			using detail::index_pair;
 			using network_info = detail::network_info<N>;
+			using detail::index_pair;
 
 			for (index_pair ip : network_info::swap_sequence)
 			{
@@ -183,35 +205,36 @@ namespace osn
 	template <typename RandomAccessIterator>
 	constexpr void sort(RandomAccessIterator begin, RandomAccessIterator end)
 	{
-		auto size = std::distance(begin, end);
-		if (size < 2)
-			return;
-		switch (size)
+		const auto size = std::distance(begin, end);
+		if (size > 1)
 		{
-		case 2:
-			sort<2>(begin);
-			break;
-		case 3:
-			sort<3>(begin);
-			break;
-		case 4:
-			sort<4>(begin);
-			break;
-		case 5:
-			sort<5>(begin);
-			break;
-		case 6:
-			sort<6>(begin);
-			break;
-		case 7:
-			sort<7>(begin);
-			break;
-		case 8:
-			sort<8>(begin);
-			break;
-		default:
-			assert(false);
-			break;
+			switch (size)
+			{
+			case 2:
+				sort<2>(begin);
+				break;
+			case 3:
+				sort<3>(begin);
+				break;
+			case 4:
+				sort<4>(begin);
+				break;
+			case 5:
+				sort<5>(begin);
+				break;
+			case 6:
+				sort<6>(begin);
+				break;
+			case 7:
+				sort<7>(begin);
+				break;
+			case 8:
+				sort<8>(begin);
+				break;
+			default:
+				assert(false);
+				break;
+			}
 		}
 	}
 }

6ceab74819f9585d59c33148f1f50778dbfdf0a9
Marcel Pi <marcelpi97@gmail.com>
2022-01-07 23:47:08 +0100

    Updated license.

--- osn.hpp
+++ osn.hpp
@@ -1,7 +1,7 @@
 /*
 	MIT License
 	
-	Copyright (c) 2021 Marcel Pi Nacy
+	Copyright (c) 2022 Marcel Pi Nacy
 	
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
